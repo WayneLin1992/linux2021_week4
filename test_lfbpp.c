01455cad123cd4b6290bedadb8fc732a1873016b
WayneLin1992 <62940642+WayneLin1992@users.noreply.github.com>
2021-04-19 12:44:49 +0800

    Add the lock free bpp
    Solution: use the atomic function to be lock free

---
+++ test_lfbpp.c
@@ -0,0 +1,53 @@
+#include <math.h>
+#include <stdio.h>
+#include <time.h>
+#include "lfpi.h"
+
+#define PRECISION 100 /* upper bound in BPP sum */
+
+/* Use Bailey¡VBorwein¡VPlouffe formula to approximate PI */
+static void *bpp(void *arg)
+{
+    int k = *(int *) arg;
+    double sum = (4.0 / (8 * k + 1)) - (2.0 / (8 * k + 4)) -
+                 (1.0 / (8 * k + 5)) - (1.0 / (8 * k + 6));
+    double *product = malloc(sizeof(double));
+    if (product)
+        *product = 1 / pow(16, k) * sum;
+    return (void *) product;
+}
+
+int main()
+{
+    FILE *fp1 = fopen("bppscability_lockfree.txt", "w");
+    for(int j=1; j<4;j++){
+        long during = 0;
+        struct timespec time_start;
+        struct timespec time_end;
+        clock_gettime(CLOCK_MONOTONIC, &time_start);
+        int bpp_args[PRECISION + 1];
+        double bpp_sum = 0;
+        tpool_t pool = tpool_create(j);
+        tpool_future_t futures[PRECISION + 1];
+
+        for (int i = 0; i <= PRECISION; i++) {
+            bpp_args[i] = i;
+            futures[i] = tpool_apply(pool, bpp, (void *) &bpp_args[i]);
+        }
+
+        for (int i = 0; i <= PRECISION; i++) {
+            double *result = tpool_future_get(futures[i], 0 /* blocking wait */);
+            bpp_sum += *result;
+            tpool_future_destroy(futures[i]);
+            free(result);
+        }
+
+        tpool_join(pool);
+        clock_gettime(CLOCK_MONOTONIC, &time_end);
+        during = time_end.tv_nsec - time_start.tv_nsec;
+        fprintf(fp1 ,"%d\t%ld \n",j,during);
+    }
+    //printf("PI calculated with %d terms: %.15f\n", PRECISION + 1, bpp_sum);
+    return 0;
+}
+
